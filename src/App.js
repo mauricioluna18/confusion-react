cea31322874b281db093025f81e643696a80b648
Mauricio Luna <breack12@gmail.com>
2021-05-25 16:54:53 -0500

    “q”

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import "./App.css";
+import { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+//function App() {
+//   return (
+//     <div className="App">
+//       <Navbar dark color="primary">
+//         <div className="container">
+//           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+//         </div>
+//       </Navbar>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default App;
